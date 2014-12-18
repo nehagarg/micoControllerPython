ea59883296ea56d73ba3d21c337a2677de15332a
nehagarg <priyadarshini.neha@gmail.com>
2014-12-18 23:22:04 +0800

    Added joint state publishing for vrep

--- src/mico_python_controller.py
+++ src/mico_python_controller.py
@@ -28,6 +28,7 @@ from sensor_msgs.msg import (
 from geometry_msgs.msg import (
     PoseStamped
 )
+from std_msgs.msg       import Float64
 import copy
 class FollowJointTrajectoryActionServer:
     def __init__(self):
@@ -39,6 +40,11 @@ class FollowJointTrajectoryActionServer:
         self._result = FollowJointTrajectoryResult()
         self._control_rate = 10.0
         self.jointStatePublisher = rospy.Publisher('trajectory_joint_state', JointState, queue_size=10)
+        self.jointStatePublisherForVrep = []
+        for i in range(1,7):
+            self.jointStatePublisherForVrep.append(rospy.Publisher('trajectory_points_joint' + repr(i), Float64, queue_size=10) )
+        self.vrep_joint_names = {"Shoulder_Joint" : "Mico_joint1", "Arm_Joint": "Mico_joint2", "Forearm_Joint" : "Mico_joint3", "Wrist_1_Joint" : "Mico_joint4","Wrist_2_Joint":"Mico_joint5","Hand_Joint":"Mico_joint6"}
+        self.vrep_position_offset =  [3.14, -1.57, 1.57, 3.14, 0,3.14]
         #self.jointTrajectoryPublisher=rospy.Publisher('trajectory_joint_velocity',JointTrajectory,queue_size=10)
     
     def execute(self, goal):
@@ -56,6 +62,16 @@ class FollowJointTrajectoryActionServer:
         #self.jointTrajectoryPublisher.publish(goal.trajectory)
 
         for trajectory_point in trajectory_points:
+            #Publish for vrepp
+            jsVrep = JointState()
+            jsVrep.name = copy.deepcopy(joint_names)
+            jsVrep.position = trajectory_point.positions
+            for j in range(len(joint_names)):
+                jsVrep.name[j] = self.vrep_joint_names[jsVrep.name[j]]
+                vrep_joint_index = int(jsVrep.name[j][-1]) - 1
+                self.jointStatePublisherForVrep[vrep_joint_index].publish(jsVrep.position[j] + self.vrep_position_offset[vrep_joint_index])
+
+            #Publisher for real robot
             js = JointState()
             #rospy.loginfo("the length of js.name is %i" %len(js.name))
             js.name=copy.deepcopy(joint_names)
@@ -65,6 +81,8 @@ class FollowJointTrajectoryActionServer:
             i=i-1
             js.position = trajectory_point.positions
             self.jointStatePublisher.publish(js)
+
+
             control_rate.sleep()
             
         start_time = rospy.get_time()
@@ -86,5 +104,5 @@ if __name__ == '__main__':
    
     rospy.init_node('mico_controller_server')
     server = FollowJointTrajectoryActionServer()
-    #server.executeFromFile("trajecoryGoalMsg2.data")
+    server.executeFromFile("../trajecoryGoalMsg.data")
     rospy.spin()
