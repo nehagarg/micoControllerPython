834035348e93751a4f0beda5473b03b63659150c
nehagarg <priyadarshini.neha@gmail.com>
2014-12-18 16:38:09 +0800

    Added joint offsets

--- src/vrep_joint_state_converter.py
+++ src/vrep_joint_state_converter.py
@@ -13,8 +13,12 @@ import copy
 
 def jointStateConverter(vrep_joint_state):
     output = JointState()
-    output.name =["shoulder_joint", "arm_joint", "forearm_joint", "wrist_1_joint","wrist_2_joint","hand_joint"]
-    output.position = vrep_joint_state.position[0:6]
+    output.name =["Shoulder_Joint", "Arm_Joint", "Forearm_Joint", "Wrist_1_Joint","Wrist_2_Joint","Hand_Joint"]
+    output.position = list(vrep_joint_state.position[0:6])
+    position_offset = [3.14, -1.57, 1.57, 3.14, 3.14,3.14]
+    for i in range(0,6):
+        #output.position[i] = 1
+        output.position[i] = output.position[i] - position_offset[i]
     output.velocity = vrep_joint_state.velocity[0:6]
     output.effort = vrep_joint_state.effort[0:6]
     pub1.publish(output)
