387fafbbafa13a088d8c1243b0e0534d08b56313
nehagarg <priyadarshini.neha@gmail.com>
2014-12-22 19:19:20 +0800

    solved vrep rotation bugs

--- src/mico_python_controller_real.py
+++ src/mico_python_controller_real.py
@@ -4,8 +4,6 @@ import roslib; roslib.load_manifest('mico_controller_python')
 import rospy
 import actionlib
 from pprint import pprint
-
-from std_msgs.msg import String
 import pickle
 import bisect
 import sys
@@ -30,7 +28,6 @@ from sensor_msgs.msg import (
 from geometry_msgs.msg import (
     PoseStamped
 )
-from std_msgs.msg       import Float64
 import copy
 class FollowJointTrajectoryActionServer:
     def __init__(self):
@@ -41,19 +38,11 @@ class FollowJointTrajectoryActionServer:
         self._fdbk = FollowJointTrajectoryFeedback()
         self._result = FollowJointTrajectoryResult()
         self._control_rate = 10.0
-        self.jointStatePublisher = rospy.Publisher('trajectory_joint_state', JointState, queue_size=10)
-
-        self.execution = rospy.Publisher('vrep_execution', String, queue_size=10)
-
-        self.jointStatePublisherForVrep = []
-        for i in range(1,7):
-            self.jointStatePublisherForVrep.append(rospy.Publisher('trajectory_points_joint' + repr(i), Float64, queue_size=10) )
-        self.vrep_joint_names = {"Shoulder_Joint" : "Mico_joint1", "Arm_Joint": "Mico_joint2", "Forearm_Joint" : "Mico_joint3", "Wrist_1_Joint" : "Mico_joint4","Wrist_2_Joint":"Mico_joint5","Hand_Joint":"Mico_joint6"}
-        self.vrep_position_offset =  [3.14, -1.57, 1.57, 3.14, 3.14,3.14]
-        #self.jointTrajectoryPublisher=rospy.Publisher('trajectory_joint_velocity',JointTrajectory,queue_size=10)
+        ##self.jointStatePublisher = rospy.Publisher('trajectory_joint_state', JointState, queue_size=10)
+        self.jointTrajectoryPublisher=rospy.Publisher('trajectory_joint_velocity',JointTrajectory,queue_size=10)
     
     def execute(self, goal):
-        pickle.dump(goal, open("trajecoryGoalMsgVrep.data", "wb"))
+        #pickle.dump(goal, open("trajecoryGoalMsg3.data", "wb"))
         joint_names = goal.trajectory.joint_names
         trajectory_points = goal.trajectory.points
         num_points = len(trajectory_points)
@@ -64,23 +53,9 @@ class FollowJointTrajectoryActionServer:
         control_rate = rospy.Rate(self._control_rate)
         i=num_points
         rospy.loginfo("the num of point is %i" % i )
-        #self.jointTrajectoryPublisher.publish(goal.trajectory)
+        self.jointTrajectoryPublisher.publish(goal.trajectory)
 
         for trajectory_point in trajectory_points:
-            #Publish for vrepp
-            jsVrep = JointState()
-            jsVrep.name = copy.deepcopy(joint_names)
-            jsVrep.position = trajectory_point.positions
-            for j in range(len(joint_names)):
-                jsVrep.name[j] = self.vrep_joint_names[joint_names[j]]
-                vrep_joint_index = int(jsVrep.name[j][-1]) - 1
-                print vrep_joint_index
-                if vrep_joint_index == 4:
-                     self.jointStatePublisherForVrep[vrep_joint_index].publish((-1*jsVrep.position[j]) + self.vrep_position_offset[vrep_joint_index])
-                else:
-                    self.jointStatePublisherForVrep[vrep_joint_index].publish(jsVrep.position[j] + self.vrep_position_offset[vrep_joint_index])
-
-            #Publisher for real robot
             js = JointState()
             #rospy.loginfo("the length of js.name is %i" %len(js.name))
             js.name=copy.deepcopy(joint_names)
@@ -88,10 +63,9 @@ class FollowJointTrajectoryActionServer:
                 #rospy.loginfo("j is %i" % j)
                 js.name[j] = js.name[j]+'_'+str(i)
             i=i-1
-            js.position = trajectory_point.positions
-            self.jointStatePublisher.publish(js)
-
-
+            #js.position = trajectory_point.positions
+            #js.velocity=trajectory_point.velocities
+            #self.jointStatePublisher.publish(js)
             control_rate.sleep()
             
         start_time = rospy.get_time()
@@ -101,8 +75,6 @@ class FollowJointTrajectoryActionServer:
         self._result.error_code = self._result.SUCCESSFUL
         self.server.set_succeeded(self._result)
 
-        rospy.sleep(5)
-        self.execution.publish("OK")
 
 
     def executeFromFile(self, filename):
@@ -115,5 +87,5 @@ if __name__ == '__main__':
    
     rospy.init_node('mico_controller_server')
     server = FollowJointTrajectoryActionServer()
-    #server.executeFromFile("../trajecoryGoalMsg.data")
+    #server.executeFromFile("trajecoryGoalMsg2.data")
     rospy.spin()
--- src/vrep_joint_state_converter.py
+++ src/vrep_joint_state_converter.py
@@ -12,15 +12,28 @@ import numpy
 import copy
 
 def jointStateConverter(vrep_joint_state):
+    vrep_joint_names = {"Shoulder_Joint" : "Mico_joint1", "Arm_Joint": "Mico_joint2", "Forearm_Joint" : "Mico_joint3", "Wrist_1_Joint" : "Mico_joint4","Wrist_2_Joint":"Mico_joint5","Hand_Joint":"Mico_joint6"}
+    joint_names = list(vrep_joint_state.name)
+    position_offset = [3.14, -1.57, 1.57, 3.14, 3.14,3.14]
     output = JointState()
     output.name =["Shoulder_Joint", "Arm_Joint", "Forearm_Joint", "Wrist_1_Joint","Wrist_2_Joint","Hand_Joint"]
-    output.position = list(vrep_joint_state.position[0:6])
-    position_offset = [3.14, -1.57, 1.57, 3.14, 0,3.14]
-    for i in range(0,6):
+    output.position = [0] * len(output.name)
+    output.velocity = [0] * len(output.name)
+    output.effort = [0] * len(output.name)
+
+    for i in range(0,len(joint_names)):
+        if joint_names[i] in vrep_joint_names.values():
+            vrep_joint_index = int(joint_names[i][-1]) - 1
+            output.position[vrep_joint_index] = vrep_joint_state.position[i] - position_offset[vrep_joint_index]
+            output.velocity[vrep_joint_index] = vrep_joint_state.velocity[i]
+            output.effort[vrep_joint_index] = vrep_joint_state.effort[i]
+    #output.position = list(vrep_joint_state.position[0:6])
+    output.position[4] = -1*output.position[4]
+    #for i in range(0,6):
         #output.position[i] = 1
-        output.position[i] = output.position[i] - position_offset[i]
-    output.velocity = vrep_joint_state.velocity[0:6]
-    output.effort = vrep_joint_state.effort[0:6]
+     #   output.position[i] = output.position[i] - position_offset[i]
+    #output.velocity = vrep_joint_state.velocity[0:6]
+    #output.effort = vrep_joint_state.effort[0:6]
     pub1.publish(output)
 
 if __name__ == '__main__':
